a596cf09173879218fcf8d91c874c67fd258a126
Clarity <clarityzhang@gmail.com>
2016-10-09 11:21:26 +0800

    add hyperleger explorer

--- client/main.go
+++ client/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/astaxie/beego"
 	"github.com/beego/i18n"
 	_ "github.com/wutongtree/notarization/client/routers"
 )
 
+func writeHyperledgerExplorer() {
+	hyperledger_explorer := beego.AppConfig.String("hyperledger_explorer")
+	filename := "static/explorer/hyperledger.js"
+	fout, err := os.Create(filename)
+	defer fout.Close()
+
+	if err != nil {
+		fmt.Printf("Write hyperledger exploer error: %v\n", err)
+	} else {
+		content := fmt.Sprintf("const REST_ENDPOINT = \"%v\";", hyperledger_explorer)
+		fout.WriteString(content)
+		fmt.Printf("Write hyperledger explorer with: %v\n", hyperledger_explorer)
+	}
+}
+
 func main() {
+	// Write hyperledger explorer config
+	writeHyperledgerExplorer()
+
 	// Register template functions.
 	beego.AddFuncMap("i18n", i18n.Tr)
 
